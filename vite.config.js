490f289ff5ed145ec65bb1066b1a59f4fc4b0b07
Ahmer-kun <157270993+Ahmer-kun@users.noreply.github.com>
2025-08-08 15:09:40 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/REACT-e-plantShopping/",
-
+  base: '/REACT-e-plantShopping/',
+build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  publicDir: 'public'
 })
